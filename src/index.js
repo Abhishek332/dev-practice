6950b3cefb25a17857fe7d217dfdad178daff170
abhishek332 <itsabhiofficial.21@gmail.com>
2022-12-09 22:10:34 +0530

    configure preloadJS and styleCss

--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-const createWindow = () => {
-	const screen = new BrowserWindow({
+function createWindow() {
+	const win = new BrowserWindow({
 		width: 800,
 		height: 600,
-		backgroundColor: '#gff55d',
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+		},
 	});
 
-	screen.loadFile(`${__dirname}/index.html`);
-	screen.menuBarVisible = false;
-};
+	win.loadFile(`${__dirname}/index.html`);
+
+	ipcMain.handle('dark-mode:toggle', () => {
+		if (nativeTheme.shouldUseDarkColors) {
+			nativeTheme.themeSource = 'light';
+		} else {
+			nativeTheme.themeSource = 'dark';
+		}
+		return nativeTheme.shouldUseDarkColors;
+	});
+
+	ipcMain.handle('dark-mode:system', () => {
+		nativeTheme.themeSource = 'system';
+	});
+}
 
 app.whenReady().then(() => {
 	createWindow();
+
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
+});
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
