320208a0d1de30f0be4c909a7175a002fa191a15
abhishek332 <itsabhiofficial.21@gmail.com>
2023-01-21 20:37:32 +0530

    add preload.js for popup window

--- src/menuOptions.js
+++ src/menuOptions.js
@@ -1,55 +1,56 @@
-const isMac = require("./utils/isMac");
-const { BrowserWindow } = require("electron");
-const path = require("path");
+const isMac = require('./utils/isMac');
+const { BrowserWindow } = require('electron');
+const path = require('path');
 
 const webHoster = () => {
-  const win = new BrowserWindow({
-    width: 400,
-    height: 250,
-    modal: true,
-
-    frame: false,
-  });
-  // win.removeMenu();
-  win.loadFile(path.join(__dirname, "webHoster.html"));
+	const win = new BrowserWindow({
+		width: 400,
+		height: 250,
+		modal: true,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+		},
+		frame: false,
+	});
+	win.loadFile(path.join(__dirname, 'webHoster.html'));
 };
 
 const menuOptions = [
-  {
-    label: "File",
-    submenu: [isMac() ? { role: "close" } : { role: "quit" }],
-  },
-  {
-    label: "Host Web",
-    submenu: [
-      {
-        label: "New Host",
-        click: webHoster,
-      },
-      { type: "separator" },
-      { label: "See Hosts" },
-    ],
-  },
-  {
-    label: "Go Remote",
-  },
-  {
-    label: "File Sharing",
-  },
-  { role: "viewMenu" },
-  {
-    role: "help",
-    submenu: [
-      {
-        label: "Learn More",
-        click: async () => {
-          const { shell } = require("electron");
-          await shell.openExternal("https://electronjs.org");
-          shell.beep();
-        },
-      },
-    ],
-  },
+	{
+		label: 'File',
+		submenu: [isMac() ? { role: 'close' } : { role: 'quit' }],
+	},
+	{
+		label: 'Host Web',
+		submenu: [
+			{
+				label: 'New Host',
+				click: webHoster,
+			},
+			{ type: 'separator' },
+			{ label: 'See Hosts' },
+		],
+	},
+	{
+		label: 'Go Remote',
+	},
+	{
+		label: 'File Sharing',
+	},
+	{ role: 'viewMenu' },
+	{
+		role: 'help',
+		submenu: [
+			{
+				label: 'Learn More',
+				click: async () => {
+					const { shell } = require('electron');
+					await shell.openExternal('https://electronjs.org');
+					shell.beep();
+				},
+			},
+		],
+	},
 ];
 
 module.exports = menuOptions;
